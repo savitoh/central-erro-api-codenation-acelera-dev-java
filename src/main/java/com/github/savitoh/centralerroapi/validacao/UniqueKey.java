bf9d8d1f11d7db422b21ac5a5bdc39fff0cbb887
savitoh <savioraires10@gmail.com>
2020-08-20 22:07:19 -0300

    Extende o atributo target da anotacoes de validacoes customizadas

--- src/main/java/com/github/savitoh/centralerroapi/validacao/UniqueKey.java
+++ src/main/java/com/github/savitoh/centralerroapi/validacao/UniqueKey.java
@@ -6,10 +6,10 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target(FIELD)
+@Target({FIELD, CONSTRUCTOR, METHOD})
 @Retention(RUNTIME)
 @Constraint(validatedBy = UniqueKeyValidator.class)
 @Documented
