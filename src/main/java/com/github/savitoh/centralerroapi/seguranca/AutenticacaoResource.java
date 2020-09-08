89cec3f886141893efe4f8ca245124d09fc869bb
savitoh <savioraires10@gmail.com>
2020-09-08 00:03:36 -0300

    Implementação da documentação de OPEN API

--- src/main/java/com/github/savitoh/centralerroapi/seguranca/AutenticacaoResource.java
+++ src/main/java/com/github/savitoh/centralerroapi/seguranca/AutenticacaoResource.java
@@ -1,23 +1,29 @@
 package com.github.savitoh.centralerroapi.seguranca;
 
+import com.github.savitoh.centralerroapi.exception.payload.ApiErrorResponsePayload;
 import com.github.savitoh.centralerroapi.seguranca.jwt.TokenManager;
 import com.github.savitoh.centralerroapi.seguranca.payload.LoginRequestPayload;
 import com.github.savitoh.centralerroapi.seguranca.payload.TokenResponsePayload;
 import com.github.savitoh.centralerroapi.seguranca.propriedades.TipoToken;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@Tag(name = "Autenticação API", description = "API para autenticação de usuários.")
 public class AutenticacaoResource {
 
 
@@ -30,13 +36,22 @@ public class AutenticacaoResource {
         this.tokenManager = tokenManager;
     }
 
-    @PostMapping
-    public ResponseEntity<TokenResponsePayload> autenticar(@Valid @RequestBody LoginRequestPayload loginRequestPayload) {
-
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @Operation(summary = "Cria token JWT para autenticação na aplicação.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Token Criado com sucesso.",
+                    content = @Content(schema = @Schema(implementation = TokenResponsePayload.class))),
+            @ApiResponse(responseCode = "400", description = "Payload recebido não contempla as regras de validação.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class))),
+            @ApiResponse(responseCode = "401", description = "Não foi possivél autenticar a requisição.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class)))
+    })
+    public TokenResponsePayload autenticar(
+            @Parameter(description = "Payload Login Autenticação") @Valid @RequestBody LoginRequestPayload loginRequestPayload) {
         UsernamePasswordAuthenticationToken authenticationToken = loginRequestPayload.toUsernamePasswordAuthenticationToken();
         Authentication authentication = authManager.authenticate(authenticationToken);
         final String jwt = tokenManager.gerarToken(authentication);
-        TokenResponsePayload tokenResponse = new TokenResponsePayload(TipoToken.BEARER.getDescricao(), jwt);
-        return ResponseEntity.ok(tokenResponse);
+        return new TokenResponsePayload(TipoToken.BEARER.getDescricao(), jwt);
     }
 }
--- src/main/java/com/github/savitoh/centralerroapi/usuario/UsuarioResource.java
+++ src/main/java/com/github/savitoh/centralerroapi/usuario/UsuarioResource.java
@@ -2,10 +2,20 @@ package com.github.savitoh.centralerroapi.usuario;
 
 import com.github.savitoh.centralerroapi.event.RecursoCriadoEvent;
 import com.github.savitoh.centralerroapi.exception.RecursoNaoEncontradoException;
+import com.github.savitoh.centralerroapi.exception.payload.ApiErrorResponsePayload;
 import com.github.savitoh.centralerroapi.usuario.payload.NovoUsuarioRequestPayload;
 import com.github.savitoh.centralerroapi.usuario.payload.UsuarioResponsePayload;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +25,8 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "Usuário API", description = "API de criação e recuperação Usuários.")
+@SecurityRequirement(name = "auth-scheme")
 public class UsuarioResource {
 
     private final UsuarioRepository usuarioRepository;
@@ -32,16 +44,35 @@ public class UsuarioResource {
         this.publisher = publisher;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UsuarioResponsePayload> getUserById(@PathVariable("id") Integer id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Recupera um usuário pelo identificador.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário recuperado.",
+                    content = @Content(schema = @Schema(implementation = UsuarioResponsePayload.class))),
+            @ApiResponse(responseCode = "401", description = "Não foi possivél autenticar a requisição.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class))),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class))),
+    })
+    public ResponseEntity<UsuarioResponsePayload> getUserById(
+            @Parameter(description="Identificador do usuário a ser recuperado", required=true) @PathVariable("id") Integer id) {
         return usuarioRepository.findById(id)
             .map(user -> ResponseEntity.ok().body(user.toDetalheUserResponsePayload()))
             .orElseThrow(() -> new RecursoNaoEncontradoException(String.format("Usuário com identificador: %s não encontrado", id)));
     }
 
-    @PostMapping
-    public ResponseEntity<String> criarUsuario(@RequestBody @Valid NovoUsuarioRequestPayload novoUsuarioRequestPayload,
-                                               HttpServletResponse response) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Cria um novo usuário.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário Criado."),
+            @ApiResponse(responseCode = "400", description = "Payload recebido não contempla as regras de validação.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class))),
+            @ApiResponse(responseCode = "401", description = "Não foi possivél autenticar a requisição.",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponsePayload.class)))
+    })
+    public ResponseEntity<Void> criarUsuario(
+            @Parameter(description="Payload Novo usuário") @RequestBody @Valid NovoUsuarioRequestPayload novoUsuarioRequestPayload,
+            HttpServletResponse response) {
         Usuario usuario = novoUsuarioRequestPayload.toUser(passwordEncoder);
         usuarioRepository.save(usuario);
         publisher.publishEvent(new RecursoCriadoEvent<>(this, response, usuario.getId()));
