dec6545726ade098c83c494cc07451aa35c772bb
Sávio Raires <savioraires10@gmail.com>
2021-03-28 02:02:01 -0300

    Refatorando consulta dinâmica

--- src/main/java/com/github/savitoh/centralerroapi/common/BaseSpecification.java
+++ src/main/java/com/github/savitoh/centralerroapi/common/BaseSpecification.java
@@ -1,18 +1,20 @@
 package com.github.savitoh.centralerroapi.common;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 
 import java.util.Objects;
-import java.util.Optional;
 
 public abstract class BaseSpecification<T, F> {
 
     public abstract Specification<T> of(F filter);
 
-    protected Optional<Specification<T>> conjuctionPredicateIfNull(final Object value) {
+    protected Specification<T> specificationOrDefaultIfNull(final Object value, @NonNull final Specification<T> specification) {
+        Assert.notNull(specification, "[Assertion failed] - this argument is required; it must not be null");
         if(Objects.isNull(value)) {
-            return Optional.of((root, query, criteriaBuilder) -> criteriaBuilder.conjunction());
+            return (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
         }
-        return Optional.empty();
+        return specification;
     }
 }
